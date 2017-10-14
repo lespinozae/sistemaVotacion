ae1c479654ce27fb6faa515dde39b748dbc7e566
Luis Manuel Espinoza <lmanuel2007@yahoo.es>
2017-10-14 14:23:35 -0600

    Actualizando Boostrap

--- header.php
+++ header.php
@@ -4,9 +4,10 @@
     <meta charset="utf-8">
     <title>Sistema de votacion</title>
     <!-- Librerias de boostrap 4 -->
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 
   </head>
-  <body> 
+  <body>
