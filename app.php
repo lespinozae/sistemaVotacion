d0131b02f571d7b62cd97fc4776ebacee214ae4e
Luis Manuel Espinoza <lmanuel2007@yahoo.es>
2017-10-18 15:48:32 -0600

    actu

--- app.php
+++ app.php
@@ -1,22 +1,25 @@
 <?php
 require_once "headerp.php";
  ?>
-<form class="form-horizontal" action="/action_page.php">
+<form class="form-horizontal" enctype="multipart/form-data" method="post" action="app.php">
   <div class="form-group">
     <label class="control-label col-sm-2" for="email">Nombre del proyecto:</label>
     <div class="col-sm-10">
-      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
-    </div>
-  </div>
-  <div class="form-group">
-    <label class="control-label col-sm-2" for="pwd">Categoria:</label>
-    <div class="col-sm-10">
-      <input type="file" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
+      <input type="email" class="form-control" id="email" placeholder="Enter email" name="nproyect">
     </div>
   </div>
   <div class="form-group">
     <label class="control-label col-sm-2" for="pwd">Imagen:</label>
     <div class="col-sm-10">
+      <input type="file" class="form-control" id="pwd" placeholder="Enter password" name="img">
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="pwd">Categoria:</label>
+    <div class="col-sm-10">
+      <select name="cat" required>
+        <option value=""></option>
+      </select>
       <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
     </div>
   </div>
